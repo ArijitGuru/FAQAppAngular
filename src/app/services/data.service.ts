cccf11258c184305f9cd301958486aa354bde625
arijit adhikary <arijitadhikary@gmail.com>
2020-02-01 20:18:00 -0500

    updates

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,8 +9,9 @@ export class DataService {
   questions: Question[];
   constructor() { 
     
-    
+    /*
     this.questions = [
+     
       {
         text: 'What is your name?',
         answer: 'Arijit',
@@ -28,38 +29,34 @@ export class DataService {
       }
 
     ];
-    
+    */
   }
 
   //Get Questions from localStrorage
   getQuestions(){
-    // if (localStorage.getItem('questions') === null){
-    //   this.questions = [];
-    // }else{
-    //   this.questions = JSON.parse(localStorage.getItem('questions'));
-    // }
+    if (localStorage.getItem('questions') === null){
+       this.questions = [];
+     }else{
+       this.questions = JSON.parse(localStorage.getItem('questions'));
+     }
     return this.questions;
   }
 
   //Add Questions to localStorage
-  addQuestion(question:Question){
-    this.questions.unshift(question);
+  addQuestion(question: Question){
+    //this.questions.unshift(question);
 
-    // let questionsLocal;
-    // if (localStorage.getItem('questions') === null){
+    let questionsLocal;
+     if (localStorage.getItem('questions') === null){
+      questionsLocal = [];
+      questionsLocal.unshift(question);
+      localStorage.setItem('questions', JSON.stringify(questionsLocal));
 
-    //   questionsLocal = [];
-    //   questionsLocal.unshift(question);
-    //   localStorage.setItem('questions', JSON.stringify(questionsLocal));
-
-    // }else{
-    //   questionsLocal = JSON.parse(localStorage.getItem('questions'));
-
-    //   questionsLocal.unshift(question);
-
-    //   localStorage.setItem('questions', JSON.stringify(questionsLocal));
-
-    // }
+     }else{
+       questionsLocal = JSON.parse(localStorage.getItem('questions'));
+       questionsLocal.unshift(question);
+       localStorage.setItem('questions', JSON.stringify(questionsLocal));
+     }
 
   }
 
@@ -68,6 +65,7 @@ export class DataService {
     for (let i=0; i<this.questions.length; i++){
       if (question == this.questions[i]){
         this.questions.splice(i, 1);
+        localStorage.setItem('questions', JSON.stringify(this.questions));
       }
     }
   }
